55188cc5baf396c57d17986b158be70ff3498cfc
dbswl067 <76262688+dbswl067@users.noreply.github.com>
2020-12-26 20:50:19 +0900

    Beakjoon

---
+++ 3단계/2단계 A+B-3.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+
+        int n = s.nextInt();
+        int arr[] = new int[n];
+
+        for(int i = 0; i < n; i++){
+            int a = s.nextInt();
+            int b = s.nextInt();
+            arr[i] = a + b;
+        }
+        for(int i = 0; i < n; i++)
+            System.out.println(arr[i]);
+
+
+    }
+}
